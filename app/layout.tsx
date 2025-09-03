583e251ded1257d888394d248a555acfebae12a4
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-09-03 21:05:35 +0300

    Initialized project

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Video Calls | Connect Instantly with High-Quality Online Meetings",
+  description:
+    "Host seamless video calls with crystal-clear audio and HD video. Enjoy secure, reliable, and user-friendly online meetings for teams, friends, and clients.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
