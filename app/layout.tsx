342f9c9c4323354f85669635d2b97acf0f711e29
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-09-06 12:35:25 +0300

    Add Vercel Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/next";
 
 import { Navbar } from "@/components/navbar";
 import { Toaster } from "@/components/ui/sonner";
@@ -40,6 +41,7 @@ export default function RootLayout({
             </main>
           </ClientProvider>
           <Toaster />
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
